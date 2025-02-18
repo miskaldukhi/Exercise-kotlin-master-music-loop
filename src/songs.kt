e40c3164ed097c020a2a57b9c87c9b2baaf6e098
miskaldukhi <miskaldukhi@gmail.com>
2025-02-18 12:57:42 +0300

    songs

--- src/songs.kt
+++ src/songs.kt
@@ -2,9 +2,15 @@ fun main(){
 val songs:MutableList<String> = mutableListOf("Smoke Sprite",
     "Luther","Miss Understood")
     playAll(songs)
+    addSong("Marilyn Monroe",songs)
 }
 fun playAll(songs:List<String>){
  for (songs in songs){
      println("Now Playing: $songs")
  }
+}
+fun addSong(songTitle:String,songs:MutableList<String>){
+    songs.add(songTitle)
+    println("Now Playing: $songTitle")
+
 }
